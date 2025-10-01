3764682761c556167bf20b0a74df263545c62813
Davyusow <dayvsonf27@gmail.com>
2025-10-01 00:58:25 -0300

    Adição dos arquivos do primeiro exercício

---
+++ exercicios/ex001.js
@@ -0,0 +1,5 @@
+alert('Bom dia!'); // mensagem de alerta no navegador
+alert('Davyusow');
+
+document.body.innerHTML = 'Bom dia!'; // Substitui todo o body do html pelo valor atribuido
+document.body.innerHTML = 'Bom dia, Davyusow!'; 
