bffeb50fd15be02921441899fec7e9f7df54146e
Davyusow <dayvsonf27@gmail.com>
2025-10-04 16:14:13 -0300

    Adição dos arquivos do segundo exercício

--- exercicios/ex001.js
+++ exercicios/ex001.js
@@ -2,4 +2,4 @@ alert('Bom dia!'); // mensagem de alerta no navegador
 alert('Davyusow');
 
 document.body.innerHTML = 'Bom dia!'; // Substitui todo o body do html pelo valor atribuido
-document.body.innerHTML = 'Bom dia, Davyusow!'; 
+document.body.innerHTML = 'Bom dia, Davyusow!';
---
+++ exercicios/ex002.js
@@ -0,0 +1,19 @@
+console.log(10 + 8 * 3 + 5)
+console.log((10 + 8 * 3 + 5) * 3)
+
+console.log(18.5 + 2 * 7.5)
+console.log((18.5 + 2 * 7.5) * 1.1) // 10% de taxa
+console.log(Math.round((1850 + 2 * 750) * 1.2) / 100) // 20% de taxa
+
+
+console.log(Math.round((1899 + 499 + 2095 + 799) * 1.1) / 100)
+
+console.log(Math.ceil(2.2))
+
+console.log(Math.floor(2.8)) 
+
+console.log(((25 * 9) /5) + 32) 
+
+console.log(((86 - 32) * 5) / 9) 
+
+console.log(((-5 * 9) / 5) + 32) 
