4e0f8c956798b121dded2763c100fcf294f46698
Davyusow <dayvsonf27@gmail.com>
2025-10-05 14:51:39 -0300

    Adição dos arquivos do exercício 3

---
+++ exercicios/ex003.js
@@ -0,0 +1,19 @@
+console.log('Meu nome é: dayvson');
+console.log('Meu nome é:' + 'Dayvson');
+console.log('Custo total: R$' + (3 + 5));
+console.log(`Custo total: R$${(3 + 5)}`);
+
+alert(`Custo total: R$${(3 + 5)}`);
+
+console.log('Custo total: R$' + (Math.round(599 + 295) / 100));
+console.log(`Custo total: R$${Math.round(599 + 295) / 100}`);
+
+alert(`Custo total: R$${Math.round(599 + 295) / 100}`);
+alert(`Custo total: R$${Math.round(599 + 295) / 100}\nObrigado e Volte Sempre!`);
+
+console.log(`Itens(${2 + 2}): R$` + (Math.round(2095 * 2 + 799 * 2) / 100))
+console.log('Frete: R$' + (Math.round(499 + 499) / 100))
+console.log('Total antes da taxa: R$' + ((Math.round(2095 * 2 + 799 * 2) / 100) + (Math.round(499 + 499) / 100)))
+console.log('Tava estimada(10%): ' + (((Math.round((2095 * 2 + 799 * 2 + 499 + 499) * 0.1) / 100))))
+
+
